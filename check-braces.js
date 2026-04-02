5823473e1cab16d30967b277c81e65db57faf6ea
ShanMaqboolFab <shan.maqbool@fabtechsol.com>
2026-04-03 01:43:56 +0500

    build: add latest without apk

--- check-braces.js
+++ check-braces.js
@@ -4,14 +4,14 @@ const content = fs.readFileSync('c:\\Users\\Shan Maqbool\\AndroidStudioProjects\
 let depth = 0;
 const lines = content.split('\n');
 
-for(let i=0; i<lines.length; i++) {
+for (let i = 0; i < lines.length; i++) {
   const line = lines[i];
-  for(let j=0; j<line.length; j++) {
-    if(line[j] === '{') depth++;
-    else if(line[j] === '}') depth--;
+  for (let j = 0; j < line.length; j++) {
+    if (line[j] === '{') depth++;
+    else if (line[j] === '}') depth--;
   }
   if (depth < 0) {
-    console.log(`EXTRA CLOSING BRACE AT LINE ${i+1}`);
+    console.log(`EXTRA CLOSING BRACE AT LINE ${i + 1}`);
   }
 }
 console.log(`Final depth: ${depth}`);
